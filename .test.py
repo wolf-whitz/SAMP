9b464d5d0577493bd44850bc00da42c91ced025d
whitzscott <whitzscott@gmail.com>
2025-11-24 18:17:53 +0800

    a

--- .test.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:8000/detect"
-data = {"text": "f2ck niggers im so gay", "threshold": 0.72, "block": []}
-
-response = requests.post(url, json=data)
-print(response.json())
