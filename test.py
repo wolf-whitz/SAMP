80727aa8b70971c22937158fece342e9acbb05da
whitzscott <whitzscott@gmail.com>
2025-11-25 17:08:07 +0800

    Your commit message

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "/detect"
+url = "https://throat-residents-handhelds-member.trycloudflare.com/detect"
 data = {"text": "some test text"}
 resp = requests.post(url, json=data)
 print(resp.json())
